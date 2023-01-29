62ac2a62e711986c4114f9a97b55ea9c7ec60ef6
Mahardika Pratama <117805307+MahardikaPratama@users.noreply.github.com>
2023-01-29 08:09:17 +0700

    Update Soal5_BukaTutupJalan.java

--- Pertemuan 2/Soal 5 - Buka Tutup Jalan/Soal5_BukaTutupJalan.java
+++ Pertemuan 2/Soal 5 - Buka Tutup Jalan/Soal5_BukaTutupJalan.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 /**
 *Program akan mengambil input plat nomor 4 mobil. Jika setiap 4 mobil yang lewat digabung angkanya,
-*dan kemudian dibagi 5, sisa hasil baginya adalah 0, maka 4 mobil tersebut harus berhenti. Jika bukan 0 maka 4 mobil itu jalan.
+*kemudian hasil gabungannya di kurang 999999 dan dibagi 5, sisa hasil baginya adalah 0, maka 4 mobil tersebut harus berhenti. Jika bukan 0 maka 4 mobil itu jalan.
 *@author Mahardika Pratama
 *@since 26-01-2023
 */
@@ -23,7 +23,7 @@ public class Soal5_BukaTutupJalan {
 		//Mengkonversi string yang berisi plat nomer ke typr data long.
 		//Alasannya adalah karena type data long memiliki panjang digit 19 dan string plat nomer diatas berjumlah 16 digit.
 		long gabungan = Long.parseLong(platNomer);
-		//Manampilkan perintah pada 4 mobil sesuai perhitungan plat nomor yang digabung di bagi 5
+		//Manampilkan perintah pada 4 mobil sesuai perhitungan gabungan plat nomor dikurang 999999 di bagi 5
 		if ((gabungan -999999) % 5 == 0) {
 			System.out.println("berhenti");
 		}
