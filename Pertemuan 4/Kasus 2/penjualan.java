b4adc6ced2130057d53b0cf6fe62c1400c51eb58
Mahardika Pratama <117805307+MahardikaPratama@users.noreply.github.com>
2023-02-10 21:15:20 +0700

    Update penjualan.java

--- Pertemuan 4/Kasus 2/penjualan.java
+++ Pertemuan 4/Kasus 2/penjualan.java
@@ -1,7 +1,11 @@
 package PraktikumTekproWeek4;
 
 import java.util.Scanner;
-
+/**
+ * 
+ * @author Mahardika
+ *
+ */
 public class penjualan {
 	private String nama_produk[];
 	private int quantity[];
@@ -9,6 +13,7 @@ public class penjualan {
 	private produk makanan[];
 	private static byte id=0;
 	private byte j=0; //variabel untuk counter pesanan
+	
 	/**
 	 * @constructor
 	 */
@@ -23,15 +28,75 @@ public class penjualan {
 	 * @method untuk menampilkan menu makanan
 	 */
 	public void tampilMenuMakanan() {
+		int k = 0;
 		System.out.println("======================================");
 		System.out.println("Daftar Menu Makanan");
 		System.out.println("======================================");
 		for (int i=0; i<=id; i++) {
 			if (!isOutOfStock(i)) {
-				System.out.println(i+1+"."+makanan[i].getNama_makanan() +"["+makanan[i].getqty()+"]"+"\t\t= Rp. "+makanan[i].getharga());
+				System.out.println(k+1+"."+makanan[i].getNama_makanan() +"["+makanan[i].getqty()+"]"+"\t\t= Rp. "+makanan[i].getharga());
+				k++;
 			}
 		}
 		System.out.println("======================================");
+	}
+	/**
+	 * @method untuk menampilkan menu utama
+	 */
+	public void tampilMenuUtama() {
+		System.out.println("======================================");
+		System.out.println("Selamat Datang di Dika's Restaurant");
+		System.out.println("======================================");
+		System.out.println("[1] Tambah Pesanan");
+		System.out.println("[2] Lihat Pesanan");
+		System.out.println("[3] Hapus Pesanan");
+		System.out.println("[4] Lihat Menu Makanan");
+		System.out.println("[5] Bayar Pesanan");
+		System.out.println("[6] Keluar");
+		System.out.println("Pilih Opsi :");
+	}
+	
+	/**
+	 * @method untuk menampilkan list pesanan
+	 */
+	public void tampilPesanan() {
+		int x = 0;
+		System.out.println("======================================");
+		System.out.println("Pesanan Anda Saat Ini");
+		System.out.println("======================================");
+		System.out.println("No.\tNama Produk\tQuantity\tHarga\t\tTotal Harga");
+		for (int i=0; i<j; i++) {
+			for (int k=0; k<=id; k++) {
+				if((this.nama_produk[i].equalsIgnoreCase(makanan[k].getNama_makanan())&&(this.quantity[i] != 0))) {
+						System.out.println((x+1)+"\t"+this.nama_produk[i]+"\t\t"+quantity[i]+"\t\tRp. "+makanan[k].getharga()+"\tRp. "+total_harga[i]);
+						x++;
+				}
+			}
+		}
+	}
+	
+	/**
+	 * @method untuk menghapus pesanan
+	 */
+	public void hapusPesanan() {
+		Scanner input = new Scanner(System.in);
+		String nama_pesanan;
+		int i, cek=0;
+		System.out.println("Masukkan nama pesanan yang ingin dihapus : ");
+		nama_pesanan = input.next();
+		for (i=0; i<j; i++) {
+			if (nama_pesanan.equals(this.nama_produk[i])) {
+				System.out.println(this.nama_produk[i]+" berhasil dihapus dari pesanan");
+				this.nama_produk[i] = "";
+				this.quantity[i] = 0;
+				this.total_harga[i] = 0;
+				cek = 1;
+				break;
+			}
+		}
+		if(cek == 0) {
+			System.out.println("Pesanan Tidak Ditemukan : ");
+		}
 	}
 	
 	/**
@@ -45,30 +110,44 @@ public class penjualan {
 		makanan[id] = new produk(nama_makanan, harga_makanan, stok); //Membuat objek menupada indeks id dari class Menu
 	}
 	
+	
 	/**
 	 * @method untuk fitur pemesanan dan mengurangi setiap stok yang ada
-	 * @param nama_pesanan
-	 * @param jumlah
 	 */
-	public void pesanMakanan(String nama_pesanan, int jumlah) {
+	public void pesanMakanan() {
+		Scanner input = new Scanner(System.in);
+		String nama_pesanan; 
+		int jumlah;
+		char lagi = 'y';
 		String cek_pesanan = "Tidak ada";
 		int i;
-		for (i=0; i<=id; i++) {
-			if (nama_pesanan.equalsIgnoreCase(makanan[i].getNama_makanan())) {
-				cek_pesanan = "ada";
-				if (makanan[i].getqty() - jumlah < 0) {
-					System.out.println("Stok makanan tidak tersedia untuk jumlah yang dipesan !");
-				}else {
-					makanan[i].setqty(jumlah);
-					this.nama_produk[j] = nama_pesanan;
-					this.quantity[j] = jumlah;
-					j++;
+		while(lagi == 'y') {
+			this.tampilMenuMakanan();
+			System.out.println("Makanan yang dipesan : ");
+			nama_pesanan = input.next();
+			System.out.println("Jumlah yang dipesan : ");
+			jumlah = input.nextInt();
+			
+			for (i=0; i<=id; i++) {
+				if (nama_pesanan.equalsIgnoreCase(makanan[i].getNama_makanan())) {
+					cek_pesanan = "ada";
+					if (makanan[i].getqty() - jumlah < 0) {
+						System.out.println("Stok makanan tidak tersedia untuk jumlah yang dipesan !");
+					}else {
+						makanan[i].setqty(jumlah);
+						this.nama_produk[j] = nama_pesanan;
+						this.quantity[j] = jumlah;
+						j++;
+						this.countTotalHarga();
+					}
+					
 				}
-				
 			}
-		}
-		if (cek_pesanan.equals("Tidak ada")) {
-			System.out.println("Makanan yang dipesan tidak tersedia di menu!");
+			if (cek_pesanan.equals("Tidak ada")) {
+				System.out.println("Makanan yang dipesan tidak tersedia di menu!");
+			}
+			System.out.println("Pesan makanan lain ? (y/t)");
+			lagi = input.next().charAt(0);
 		}
 		
 	}
@@ -92,6 +171,7 @@ public class penjualan {
 	 * @method untuk melakukan proses pembayaran
 	 */
 	public void pembayaran() {
+		Scanner input = new Scanner(System.in);
 		double total_bayar=0;
 		double uang, kembalian;
 		System.out.println("========================================================================");
@@ -112,7 +192,6 @@ public class penjualan {
 		System.out.println("=========================================================================\n");
 		if(j>0) {
 			System.out.println("Masukkan jumlah uang anda");
-			Scanner input = new Scanner(System.in);
 			uang = input.nextDouble();
 			kembalian = uang - total_bayar;
 			if (kembalian>=0) {
@@ -125,8 +204,6 @@ public class penjualan {
 				System.out.println("Pembayaran gagal dikarenakan uang anda tidak mencukupi !");
 			}
 		}
-		
-		
 	}
 	/**
 	 * 
